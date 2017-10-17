1e63dfc306f3c217c41f848d43440333cbe3bdc5
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2017-10-17 18:14:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ var pool=new Pool(config);
 
 app.get('/test-db',function(req,res){
     //make a request
-    var name='hari2';
+    var name='hari3';
     pool.query(`INSERT INTO "user" ("name", "mob", "game", "place", "lat", "long")
-VALUES ('hari2', '', NULL, NULL, '12.3', '16.5');`,function(err,result){
+VALUES ('$1', '', NULL, NULL, '12.3', '16.5');`,[name],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
