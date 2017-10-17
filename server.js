9b8d974542938c9748681db97a24ee409fbe40ca
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2017-10-17 18:09:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
+
+var Pool=require('pg').Pool;
+var config={
+   user:'hariharanm2016',
+   database:'hariharanm2016',
+   host:'db.imad.hasura-app.io',
+   port:'5432',
+   password:process.env.DB_PASSWORD
+};
+var pool=new Pool(config);
+
+app.get('/test-db',function(req,res){
+    //make a request
+    var name='hari2';
+    var pass='value';
+    pool.query("insert into 'user'(name,mob) values($1)",[name],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send('Success');
+        }
+    });
+    //respond with data
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
