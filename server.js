1e6892579bf6fa856e0082a4683ad2510c0f26e2
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2017-10-25 17:21:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/select_game',function(req,res){
                 else{
                     for(var i=0; i< result.rows.length; i++){
                         console.log('\n'+JSON.stringify(result.rows[i])+'\n');
-                        if(getDistanceFromLatLonInKm(result.rows[i].ylat, result.rows[i].ylong, result.rows[i].flat, result.rows[i].flong)<=0.1){
+                        if(getDistanceFromLatLonInKm(result.rows[i].ylat, result.rows[i].ylong, result.rows[i].flat, result.rows[i].flong)<=100){
                         console.log('Distance= '+getDistanceFromLatLonInKm(result.rows[i].ylat, result.rows[i].ylong, result.rows[i].flat, result.rows[i].flong).toString());
                             sample.push(result.rows[i]);
                         }
